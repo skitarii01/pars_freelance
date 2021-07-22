dcb75fc26db25228ef7c8d82cdf23ff5571277af
de_bug <44062411+skitarii01@users.noreply.github.com>
2021-07-22 14:17:49 +0300

    Add files via upload

---
+++ main_freelance.py
@@ -0,0 +1,159 @@
+import time
+import threading
+import requests
+from bs4 import BeautifulSoup
+import telebot
+from telebot import types
+
+TOKEN = ''
+with open('TOKEN.txt') as fl:
+    for i in fl:
+        TOKEN = i
+        break
+bot = telebot.TeleBot(TOKEN)
+
+bot = telebot.TeleBot(TOKEN)
+
+
+def get_descr(id):
+    descr = []
+    classq = 'task__description'
+
+    id = '/tasks/' + id
+    resp = requests.get('https://freelance.habr.com' + id)
+    soup = BeautifulSoup(resp.text, 'lxml')
+
+    aq = soup.find_all('div')
+
+    for i in aq:
+        if i.has_attr('class') and i['class'][0] == classq:
+            for k in i.descendants:
+                if k.string != None and not ([k.string, 0] in descr or [k.string, 1] in descr):
+                    if k.name == 'li':
+                        descr.append([k.string, 1])
+                    else:
+                        descr.append([k.string, 0])
+    s = ''
+    for i in descr:
+        if i[1] == 0:
+            s += i[0] + '\n'
+        elif i[1] == 1:
+            s += '- ' + i[0] + '\n\n'
+    return s
+
+
+# hello there rq
+
+def send_new(id, task):
+    keyboard = types.InlineKeyboardMarkup()
+    callback_button = types.InlineKeyboardButton(text="описание", callback_data="test")
+    keyboard.add(callback_button)
+    s = 'title: ' + task[1] + '\n\nprice: %s\n\nid: %s\n\n' % (task[2], task[0].split('/')[2])
+    bot.send_message(int(id), s, reply_markup=keyboard)
+
+
+def check_updates(id):
+    with open('tags_users.txt') as fl:
+        for i in fl:
+            par = i.split()
+            if par[0] == id:
+                url = par[1]
+    tasqs = []
+
+    with open('tasks/' + id + '.txt') as fl:
+        for i in fl:
+            tasqs.append(i)
+    resp = requests.get(url)
+    sp = BeautifulSoup(resp.content, 'lxml')
+
+    divs = sp.find_all('article')
+
+    news = []
+    for i in divs:
+        if i.has_attr('class') and i['class'] == ['task', 'task_list']:
+            ok = False
+            for k in i.descendants:
+                if k.name == 'a':
+                    if (not (k['href'] + '\n' in tasqs)) and '/tasks/' in k['href']:
+                        news.append([k['href'], k.string])
+                        ok = True
+                    elif '/tasks/' in k['href']:
+                        ok = False
+                elif k.name == 'span':
+                    if k.has_attr('class') and (k['class'][0] == 'negotiated_price' or k['class'][0] == 'count') and ok:
+                        for q in k.descendants:
+                            if str(type(q)) == "<class 'bs4.element.NavigableString'>":
+                                news[len(news) - 1].append(q.string)
+                                break
+
+    with open('tasks/' + id + '.txt', 'w') as fl:
+        for i in tasqs:
+            fl.write(i)
+        for i in news:
+            fl.write(i[0] + '\n')
+    for i in news:
+        send_new(id, i)
+    return
+
+
+def checker():
+    c = 0
+    ok = True
+    while True:
+        try:
+            print('check: ' + str(c))
+            with open('tags_users.txt') as fl:
+                for i in fl:
+                    check_updates(i.split()[0])
+            time.sleep(20)
+            c += 1
+            ok = True
+        except Exception:
+            try:
+                if not(ok):
+                    bot.send_message(450543987, 'со мной чтото не так')
+                    ok = False
+            except Exception:
+                pass
+            time.sleep(20)
+
+
+main_thr = threading.Thread(target=checker)
+main_thr.start()
+
+
+def return_to_the_title(msg):
+    time.sleep(90)
+    keyboard = types.InlineKeyboardMarkup()
+    callback_button = types.InlineKeyboardButton(text="описание", callback_data="test")
+    keyboard.add(callback_button)
+    bot.edit_message_text(chat_id=msg.chat.id, message_id=msg.message_id, text=msg.text,
+                          reply_markup=keyboard)
+    return
+
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback_inline(call):
+    try:
+        if call.message:
+            if call.data == "test":
+                # регулярку надо бы
+                id = call.message.text.split('id: ')[1].split('\n')[0]
+                bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,
+                                      text=call.message.text + '\n\ndescr: ' + get_descr(id))
+                t1 = threading.Thread(target=return_to_the_title, args=[call.message])
+                t1.start()
+    except Exception:
+        try:
+            bot.send_message(450543987, 'со мной чтото не так')
+        except Exception:
+            pass
+        time.sleep(60 ** 6)
+
+@bot.message_handler(commands=['check'])
+def text_handler(msg):
+    bot.send_message(msg.from_user.id, 'i am fine')
+    print(msg.from_user.id)
+
+
+bot.polling()
